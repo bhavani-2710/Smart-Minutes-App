04879194c0ac430bce91496f88008545fd812259
Bhavani Murali <bhavani9271@gmail.com>
2025-07-24 17:28:47 +0530

    update profile modal animation from slide to fade

--- frontend/app/(tabs)/profile.jsx
+++ frontend/app/(tabs)/profile.jsx
@@ -107,7 +107,7 @@ const Profile = () => {
 
           <Modal
             visible={openProfileModal}
-            animationType="slide"
+            animationType="fade"
             transparent={true}
             onRequestClose={() => setOpenProfileModal(false)}
             backdropColor={"#FFE5E5"}
